2712416bd24ba349c1ae7204ec59cdc414df2331
kaji-robe <hayano3.141592653@gmail.com>
2024-01-30 11:50:30 +0900

    2024/01/30 11:50

---
+++ src/test/java/com/techacademy/TutorialJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
