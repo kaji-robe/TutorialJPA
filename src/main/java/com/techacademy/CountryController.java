d870f54ab86be57d2fa6625df0eeddeeb016872a
kaji-robe <hayano3.141592653@gmail.com>
2024-01-31 15:26:51 +0900

    2024/01/31 15:26

--- src/main/java/com/techacademy/CountryController.java
+++ src/main/java/com/techacademy/CountryController.java
@@ -3,7 +3,10 @@ package com.techacademy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable; // 追加
+import org.springframework.web.bind.annotation.PostMapping; // 追加
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam; // 追加
 
 @Controller
 @RequestMapping("country")
@@ -22,4 +25,45 @@ public class CountryController {
         // country/list.htmlに画面遷移
         return "country/list";
     }
+
+    // ----- 追加:ここから -----
+    // ----- 詳細画面 -----
+    @GetMapping(value = { "/detail", "/detail/{code}/" })
+    public String getCountry(@PathVariable(name = "code", required = false) String code, Model model) {
+        // codeが指定されていたら検索結果、無ければ空のクラスを設定
+        Country country = code != null ? service.getCountry(code) : new Country();
+        // Modelに登録
+        model.addAttribute("country", country);
+        // country/detail.htmlに画面遷移
+        return "country/detail";
+    }
+
+    // ----- 更新（追加） -----
+    @PostMapping("/detail")
+    public String postCountry(@RequestParam("code") String code, @RequestParam("name") String name,
+            @RequestParam("population") int population, Model model) {
+        // 更新（追加）
+        service.updateCountry(code, name, population);
+
+        // 一覧画面にリダイレクト
+        return "redirect:/country/list";
+    }
+
+    // ----- 削除画面 -----
+    @GetMapping("/delete")
+    public String deleteCountryForm(Model model) {
+        // country/delete.htmlに画面遷移
+        return "country/delete";
+    }
+
+    // ----- 削除 -----
+    @PostMapping("/delete")
+    public String deleteCountry(@RequestParam("code") String code, Model model) {
+        // 削除
+        service.deleteCountry(code);
+
+        // 一覧画面にリダイレクト
+        return "redirect:/country/list";
+    }
+    // ----- 追加:ここまで -----
 }
--- src/main/java/com/techacademy/CountryService.java
+++ src/main/java/com/techacademy/CountryService.java
@@ -1,15 +1,15 @@
 package com.techacademy;
 
 import java.util.List;
+import java.util.Optional; // 追加
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional; // 追加
 
 @Service
 public class CountryService {
     private final CountryRepository repository;
 
-    @Autowired
     public CountryService(CountryRepository repository) {
         this.repository = repository;
     }
@@ -19,4 +19,28 @@ public class CountryService {
         // リポジトリのfindAllメソッドを呼び出す
         return repository.findAll();
     }
+
+    // ----- 追加:ここから -----
+    // 1件を検索して返す
+    public Country getCountry(String code) {
+        // findByIdで検索
+        Optional<Country> option = repository.findById(code);
+        // 取得できなかった場合はnullを返す
+        Country country = option.orElse(null);
+        return country;
+    }
+
+    // 更新（追加）を行なう
+    @Transactional
+    public void updateCountry(String code, String name, int population) {
+        Country country = new Country(code, name, population);
+        repository.save(country);
+    }
+
+    // 削除を行なう
+    @Transactional
+    public void deleteCountry(String code) {
+        repository.deleteById(code);
+    }
+    // ----- 追加:ここまで -----
 }
